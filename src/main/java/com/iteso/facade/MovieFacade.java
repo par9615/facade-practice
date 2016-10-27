9aa263fe11f5127ba3953e81652772b2e065263f
Miguel Hernández <par9615@gmail.com>
2016-10-27 08:12:03 -0500

    Play movie method added

--- src/main/java/com/iteso/facade/MovieFacade.java
+++ src/main/java/com/iteso/facade/MovieFacade.java
@@ -1,6 +1,7 @@
 package com.iteso.facade;
 
 import com.iteso.facade.interfaces.*;
+import com.iteso.facade.interfaces.impl.PS3Game;
 
 /**
  * Created by simio on 27/10/2016.
@@ -12,7 +13,7 @@ public class MovieFacade {
     SoundSystem soundSystem;
     Router router;
     Lights lights;
-    VideoGame game;
+    Movie movie;
 
     MovieFacade( TV tv,
                  DVDPlayer dvdPlayer,
@@ -20,7 +21,7 @@ public class MovieFacade {
             SoundSystem soundSystem,
             Router router,
             Lights lights,
-            VideoGame game){
+            Movie movie){
 
         this.tv = tv;
         this.dvdPlayer = dvdPlayer;
@@ -28,9 +29,37 @@ public class MovieFacade {
         this.soundSystem = soundSystem;
         this.router = router;
         this.lights = lights;
+        this.movie = movie;
 
 
     }
+
+
+    public void playMovie(String movieName){
+        System.out.println("Turning the system ON, this may take some time");
+        System.out.println();
+
+        tv.on();
+        tv.toHDMI();
+
+        dvdPlayer.on();
+        controller.on();
+        movie = new DVD(movieName);
+        dvdPlayer.insertMovie(movie);
+
+        soundSystem.on();
+        soundSystem.toOpticalEntry();
+
+        router.on();
+        router.checkInternet();
+
+        lights.off();
+
+        movie.play();
+        System.out.println();
+    }
+
+
 
 
 }
